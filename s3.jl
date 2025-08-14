94f22eba8e3126f4568043597e0bba13b295b7cb
Udit Varma <uditvarma@nbp-25-245-178.nbp.ruw.rutgers.edu>
2025-08-14 15:08:12 -0400

    gives promising results for p=1

--- s3.jl
+++ s3.jl
@@ -7,7 +7,7 @@ using DelimitedFiles
 using NPZ
 using ExpmV
 
-
+"""
 function von_neumann_entropy(ψ::Vector{<:Complex}, cut::Int, L::Int)
     dimA = 3^cut
     dimB = 3^(L - cut)
@@ -22,7 +22,7 @@ function von_neumann_entropy(ψ::Vector{<:Complex}, cut::Int, L::Int)
     end
     return S
 end
-
+"""
 function random_product_state(L::Int)
     product_state = nothing
 
@@ -64,7 +64,7 @@ function time_evolution(ψ::Vector{ComplexF64}, dt::Float64, L)
     
     return ψ_new
 end
-"""
+
 
 function I3(psi::Vector{ComplexF64})
     log3(x) = log(x) / log(3)
@@ -82,25 +82,98 @@ function I3(psi::Vector{ComplexF64})
     
     return SA + SB + SC + SABC - SAB - SAC - SBC
 end
+"""
+
+function Hamiltonian(L)
+    # Define Pauli matrices as complex sparse matrices
+    id = sparse(ComplexF64[1 0 0; 0 1 0; 0 0 1])
+    sx = 1/sqrt(2) * sparse(ComplexF64[0 1 0; 1 0 1; 0 1 0])
+    sy = 1/sqrt(2) * sparse(ComplexF64[0 -im 0; im 0 -im; 0 im 0])
+    sz = sparse(ComplexF64[1 0 0; 0 0 0; 0 0 -1])
+
+    sp = 1/sqrt(2) * (sx + im * sy)
+    sm = 1/sqrt(2) * (sx - im * sy)
+
+    # Preallocate vectors of operators with correct type
+    #sx_list = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L)
+    #sy_list = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L)
+    szl = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sz operators
+    sz2l = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sz² operators
+    spl = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sp operators
+    sml = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sm operators
+
+    for i_site in 1:L
+        p_ops = fill(id, L)
+        m_ops = fill(id, L)
+        z_ops = fill(id, L)
+        z2_ops = fill(id, L)
+        p_ops[i_site] = sp
+        m_ops[i_site] = sm
+        z_ops[i_site] = sz
+        z2_ops[i_site] = sz^2
+
+        # Build the full operator by tensoring
+        P = p_ops[1]
+        M = m_ops[1]
+        Z = z_ops[1]
+        Z2 = z2_ops[1]
+        for j in 2:L
+            P = kron(P, p_ops[j])
+            M = kron(M, m_ops[j])
+            Z = kron(Z, z_ops[j])
+            Z2 = kron(Z2, z2_ops[j])
+        end
+
+        spl[i_site] = P
+        sml[i_site] = M
+        szl[i_site] = Z
+        sz2l[i_site] = Z2
+    end
+
+    dim = 3^L
+    Ha = spzeros(ComplexF64, dim, dim)
+
+    for i in 1:L
+        ip = (i % L) + 1  # Periodic boundary
+        Ha += ((spl[i]^2 * sml[ip]^2 + sml[i]^2 * spl[ip]^2) + adjoint((spl[i]^2 * sml[ip]^2 + sml[i]^2 * spl[ip]^2)))
+    end
+
+    Hb = spzeros(ComplexF64, dim, dim)
+
+    for i in 1:L
+        ip = (i % L) + 1  # Periodic boundary
+        Hb += (spl[i] * szl[i] * sml[ip] * szl[ip] + sml[i] * szl[i] * spl[ip] * szl[ip] + adjoint(spl[i] * szl[i] * sml[ip] * szl[ip]) + adjoint(sml[i] * szl[i] * spl[ip] * szl[ip]))
+    end
+
+    Hc = spzeros(ComplexF64, dim, dim)
+
+    for i in 1:L
+        ip = (i % L) + 1  # Periodic boundary
+        Hc += (spl[i]^2 * spl[ip] * szl[ip] + sml[i]^2 * sml[ip] * szl[ip] + adjoint(spl[i]^2 * spl[ip] * szl[ip]) + adjoint(sml[i]^2 * sml[ip] * szl[ip]))
+    end
+
+    return Ha, Hb, Hc, sz2l, szl 
+end
 
 ### This function is for Z measurements only
 
 function Entropy_t_z(L::Int, T::Float64, dt::Float64, p::Float64, shot::Int)
     Random.seed!(shot)  # Set random seed
     s_t = random_product_state(L)
+    #s_t = neel_spin1_complex(L)  # Use the Néel state as the initial state 
     S_list = Float64[]
 
     # Define Hamiltonian and local observables
-    H, sz2l, szl = Hamiltonian(L)
+    _, _, _, sz2l, szl = Hamiltonian(L)
     sm_list = szl
     steps = Int(floor(T / dt))
 
     for _ in 1:steps
-        #push!(S_list, von_neumann_entropy(s_t, L÷2, L))
-        push!(S_list, I3(s_t))
+        #push!(S_list, entropy_vn(s_t, L, 1:L÷2)) ## Half-chain entropy
+        push!(S_list, tmi(s_t))
 
         # Time evolution
-        s_t = time_evolution(s_t, dt, L)
+        s_t = time_evolution(s_t, dt, L, shot)
         s_t /= norm(s_t)
 
         # Measurements
@@ -126,7 +199,14 @@ function Entropy_t_z(L::Int, T::Float64, dt::Float64, p::Float64, shot::Int)
     # Save result to disk
     filename = "L$(L),T$(T),dt$(dt),p$(p),dirZ,s$(shot).npy"
     npzwrite(filename, S_list)
+    
 
+    """
+    folder = "/Users/uditvarma/Documents/s3/data"
+    filename = joinpath(folder, "L$(L),T$(T),dt$(dt),p$(p),dirZ,s$(shot).npy")
+    npzwrite(filename, S_list)
+    """
+    
     return S_list
 end
 
@@ -203,7 +283,7 @@ function tmi(ψ::Vector{ComplexF64})
     SAC  = entropy_vn(ψ, L, (1:(qL)) ∪ ((2qL+1):3qL))
     SBC  = entropy_vn(ψ, L, (2qL+1):L)
 
-    return SA + SB + SC + SABC - SAB - SAC - SBC
+    return SA + SB + SC - SAB - SAC - SBC + SABC
 
 end
 
@@ -223,78 +303,10 @@ function entropy_vn(ψ::Vector{<:Complex}, L::Int, subsystem::AbstractArray{Int}
     return S
 end
 
-function Hamiltonian(L)
-    # Define Pauli matrices as complex sparse matrices
-    id = sparse(ComplexF64[1 0 0; 0 1 0; 0 0 1])
-    sx = 1/sqrt(2) * sparse(ComplexF64[0 1 0; 1 0 1; 0 1 0])
-    sy = 1/sqrt(2) * sparse(ComplexF64[0 -im 0; im 0 -im; 0 im 0])
-    sz = sparse(ComplexF64[1 0 0; 0 0 0; 0 0 -1])
 
-    sp = 1/sqrt(2) * (sx + im * sy)
-    sm = 1/sqrt(2) * (sx - im * sy)
 
-    # Preallocate vectors of operators with correct type
-    #sx_list = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L)
-    #sy_list = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L)
-    szl = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sz operators
-    sz2l = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sz² operators
-    spl = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sp operators
-    sml = Vector{SparseMatrixCSC{ComplexF64, Int}}(undef, L) # List of sm operators
-
-    for i_site in 1:L
-        p_ops = fill(id, L)
-        m_ops = fill(id, L)
-        z_ops = fill(id, L)
-        z2_ops = fill(id, L)
-        p_ops[i_site] = sp
-        m_ops[i_site] = sm
-        z_ops[i_site] = sz
-        z2_ops[i_site] = sz^2
-
-        # Build the full operator by tensoring
-        P = p_ops[1]
-        M = m_ops[1]
-        Z = z_ops[1]
-        Z2 = z2_ops[1]
-        for j in 2:L
-            P = kron(P, p_ops[j])
-            M = kron(M, m_ops[j])
-            Z = kron(Z, z_ops[j])
-            Z2 = kron(Z2, z2_ops[j])
-        end
-
-        spl[i_site] = P
-        sml[i_site] = M
-        szl[i_site] = Z
-        sz2l[i_site] = Z2
-    end
-
-    dim = 3^L
-    Ha = spzeros(ComplexF64, dim, dim)
-
-    for i in 1:L
-        ip = (i % L) + 1  # Periodic boundary
-        Ha += ((spl[i]^2 * sml[ip]^2 + sml[i]^2 * spl[ip]^2) + adjoint((spl[i]^2 * sml[ip]^2 + sml[i]^2 * spl[ip]^2)))
-    end
-
-    Hb = spzeros(ComplexF64, dim, dim)
-
-    for i in 1:L
-        ip = (i % L) + 1  # Periodic boundary
-        Hb += (spl[i] * szl[i] * sml[ip] * szl[ip] + sml[i] * szl[i] * spl[ip] * szl[ip] + adjoint(spl[i] * szl[i] * sml[ip] * szl[ip]) + adjoint(sml[i] * szl[i] * spl[ip] * szl[ip]))
-    end
-
-    Hc = spzeros(ComplexF64, dim, dim)
-
-    for i in 1:L
-        ip = (i % L) + 1  # Periodic boundary
-        Hc += (spl[i]^2 * spl[ip] * szl[ip] + sml[i]^2 * sml[ip] * szl[ip] + adjoint(spl[i]^2 * spl[ip] * szl[ip]) + adjoint(sml[i]^2 * sml[ip] * szl[ip]))
-    end
-
-    return Ha, Hb, Hc, sz2l, szl 
-end
-
-function time_evolution(ψ::Vector{ComplexF64}, dt::Float64, L)
+function time_evolution(ψ::Vector{ComplexF64}, dt::Float64, L, shot::Int)
+    Random.seed!(shot)  # Set random seed for reproducibility
     ψ /= norm(ψ)
     Ha, Hb, Hc, _, _ = Hamiltonian(L)
     a = 2π * rand()
@@ -302,6 +314,7 @@ function time_evolution(ψ::Vector{ComplexF64}, dt::Float64, L)
     c = 2π * rand()
     # Apply exp(-im * H * dt) directly to ψ
     H = a * Ha + b * Hb + c * Hc
+    #H = Ha ## To test with Pradip's TDVP code
     ψ_new = expmv(-im * dt, H, ψ)
 
     # Normalize the state
@@ -309,3 +322,24 @@ function time_evolution(ψ::Vector{ComplexF64}, dt::Float64, L)
     
     return ψ_new
 end
+
+
+# Function to generate spin-1 Néel state vector of length N as complex numbers
+function neel_spin1_complex(N::Int)
+    # Construct the Néel pattern: Up, Dn, Up, Dn...
+    neel_state = [isodd(j) ? "Up" : "Dn" for j in 1:N]
+    
+    # Start with first spin
+    psi = spin1_vector(neel_state[1])
+    
+    # Build the full tensor product
+    for j in 2:N
+        psi = kron(psi, spin1_vector(neel_state[j]))
+    end
+    
+    # Convert to complex vector
+    psi_complex = ComplexF64.(psi)  # broadcast conversion
+    
+    return psi_complex
+end
+
